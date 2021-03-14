d76c93a2252d3a83d445ad6e38e6b0957970508f
Evgeniy <yevgeniykyr5@ukr.netgit config --global user.name prostoevgeniy5git config --global user.email yevgeniykyr5@ukr.net>
2021-03-14 08:55:35 +0200

    feat: solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    var result = 0;
+    var temp;
+    if(n < 0) {
+        temp = (-1 * n).toString().split('');
+    } else {
+        temp = n.toString().split('');
+    }
+
+  for(var i = 0; i < temp.length; i++) {
+    result += Number(temp[i]) * Math.pow(10, i)
+
+  }
+  return result;
 }
